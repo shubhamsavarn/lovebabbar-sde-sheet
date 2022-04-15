d0bb6e6fe6516ca5d7d876d74365d67b11e43c30
shubhamsavarn <54256934+shubhamsavarn@users.noreply.github.com>
2022-04-15 21:29:36 +0530

    Update Merge Intervals.cpp

--- Merge Intervals.cpp
+++ Merge Intervals.cpp
@@ -1,50 +1,25 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-struct Interval
-{
-    int start, end;
-};
-bool compareInterval(Interval i1, Interval i2)
-{
-    return (i1.start < i2.start);
-}
-
-void mergeIntervals(Interval arr[], int n)
-{
-    if (n <= 0)
-        return;
-
-    stack<Interval> s;
-
-    sort(arr, arr+n, compareInterval);
-    s.push(arr[0]);
-    for (int i = 1 ; i < n; i++)
-    {
-        Interval top = s.top();
-        if (top.end < arr[i].start)
-            s.push(arr[i]);
-        else if (top.end < arr[i].end)
-        {
-            top.end = arr[i].end;
-            s.pop();
-            s.push(top);
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end());
+        int n = intervals.size();
+        vector<vector<int>> v;
+        for(int i=0;i<n;i++){
+            if(v.empty()){
+                v.push_back(intervals[i]);
+            }else{
+                
+                vector<int>&v1 = v.back();
+                int y = v1[1];
+                if(intervals[i][0] <= y){
+                    v1[1] = max(y,intervals[i][1]);
+                }else{
+                    v.push_back(intervals[i]);
+                }
+                 
+            }
+            
         }
+        return v;
     }
-
-    cout << "\n The Merged Intervals are: ";
-    while (!s.empty())
-    {
-        Interval t = s.top();
-        cout << "[" << t.start << "," << t.end << "] ";
-        s.pop();
-    }
-    return;
-}
-int main()
-{
-    Interval arr[] =  { {6,8}, {1,9}, {2,4}, {4,7} };
-    int n = sizeof(arr)/sizeof(arr[0]);
-    mergeIntervals(arr, n);
-    return 0;
-}
+};
