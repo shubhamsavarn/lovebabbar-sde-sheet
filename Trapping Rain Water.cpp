6061780c294887fa9ef4f66b088e100181380038
shubhamsavarn <54256934+shubhamsavarn@users.noreply.github.com>
2022-04-19 23:38:58 +0530

    Create Trapping Rain Water.cpp

---
+++ Trapping Rain Water.cpp
@@ -0,0 +1,26 @@
+int maxWater(int arr[], int n)
+{
+     
+    // To store the maximum water
+    // that can be stored
+    int res = 0;
+     
+    // For every element of the array
+    for (int i = 1; i < n-1; i++) {
+         
+        // Find the maximum element on its left
+        int left = arr[i];
+        for (int j=0; j<i; j++)
+           left = max(left, arr[j]);
+         
+        // Find the maximum element on its right  
+        int right = arr[i];
+        for (int j=i+1; j<n; j++)
+           right = max(right, arr[j]);
+        
+       // Update the maximum water   
+       res = res + (min(left, right) - arr[i]);  
+    }
+ 
+    return res;
+}
