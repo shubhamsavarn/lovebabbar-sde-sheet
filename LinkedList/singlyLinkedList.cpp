60316bd0703b1b9e57c7ad8244822cf8e2230fc8
shubhamsavarn <54256934+shubhamsavarn@users.noreply.github.com>
2022-04-28 22:53:21 +0530

    insertatLast

--- LinkedList/singlyLinkedList.cpp
+++ LinkedList/singlyLinkedList.cpp
@@ -26,14 +26,27 @@ void print(Node* &head){
     }
     cout << endl;
 }
+//inserting at last
+void insertatLast(Node* &tail,int data){
+    Node* temp = new Node(data);
+    tail -> next = temp;
+    temp = tail;
+}
 int main(){
     Node* node1 = new Node(8);
     //cout << node1->data << endl;
     //cout << node1->next;
     Node* head = node1;
+    Node* tail = node1;
+    
     print(head);
+    cout << "after insertion at head"<<endl;
     insertAtfirst(head,10);
     insertAtfirst(head,11);
     print(head);
+    cout << "insertion at tail"<<endl;
+    insertatLast(tail,55);
+    print(head);
     return 0;
 }
+
