36e7c3ee0b6e991fc6aed7b28abc368fad19c43d
shubhamsavarn <54256934+shubhamsavarn@users.noreply.github.com>
2022-05-03 15:38:57 +0530

    Create DoublyLinkedList.cpp

---
+++ LinkedList/DoublyLinkedList.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node*  prev;
+    Node* next;
+    Node(int d){
+        this->data = d;
+        this->prev = NULL;
+        this->next = NULL;
+    }
+};
+void print(Node* head){
+    Node* temp = head;
+    while(temp!= NULL){
+        cout << temp->data<<" "<<endl;
+        temp = temp->next;
+    }
+}
+int length(Node* head){
+    int count = 0;
+    Node* temp = head;
+    while(temp != NULL){
+        count++;
+        temp = temp->next;
+    }
+    return count;
+}
+void insertAtstart(Node* &head,int d){
+    Node* temp = new Node(d);
+    temp->next = head;
+    head->prev = temp;
+    head = temp;
+    
+}
+int main(){
+    Node* node1 = new Node(10);
+    Node* node2 = new Node(20);
+    Node* head = node1;
+    head->next = node2;
+    //print(head);
+    //cout << length(head)<<endl;
+    //cout << "Adding node at first position"<<endl;
+    insertAtstart(head,2);
+    print(head);
+    return 0;
+    
+}
