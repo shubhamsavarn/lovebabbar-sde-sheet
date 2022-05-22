034a46e078938af1764fe3427e53886afcab8fba
shubhamsavarn <54256934+shubhamsavarn@users.noreply.github.com>
2022-05-22 23:13:32 +0530

    Update Detect Loop in linked list.cpp

--- Detect Loop in linked list.cpp
+++ Detect Loop in linked list.cpp
@@ -4,6 +4,7 @@ class Solution
     //Function to check if the linked list has a loop.
     bool detectLoop(Node* head)
     {
+        //
         map<Node*,bool>visited;
         if(head == NULL){
             return false;
