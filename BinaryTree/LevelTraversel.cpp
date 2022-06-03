82330a6eff64d18f2050786ea98699de3eba58ae
shubhamsavarn <54256934+shubhamsavarn@users.noreply.github.com>
2022-06-04 02:31:16 +0530

    Create LevelTraversel.cpp

---
+++ BinaryTree/LevelTraversel.cpp
@@ -0,0 +1,25 @@
+void leveltraverse(node* root){
+    queue<node*>q;
+    q.push(root);
+    q.push(NULL);
+    while(!q.empty()){
+        node* temp = q.front();
+        q.pop();
+        if(temp == NULL){
+            cout << endl;
+            if(!q.empty()){
+                q.push(NULL);
+            }
+        }
+        else{
+            cout << temp->data<< " ";
+            if(temp->left != NULL){
+                q.push(temp->left);
+            }
+            if(temp->right != NULL){
+                q.push(temp->right);
+            }
+        }
+        
+    }
+}
