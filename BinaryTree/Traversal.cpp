e367b5a1f9969aa6084749e1d7c68e001b47b43e
shubhamsavarn <54256934+shubhamsavarn@users.noreply.github.com>
2022-06-03 23:51:34 +0530

    Create Traversal.cpp

---
+++ BinaryTree/Traversal.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int d){
+        this->data = d;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+Node* buildTree(Node* root){
+    cout << "enter the data"<<endl;
+    int data;
+    cin >>  data;
+    root = new Node(data);
+    if(data == -1){
+        return NULL;
+    }
+    cout << "enter the data for inserting in left of "<<data<<endl;
+    root->left = buildTree(root->left);
+    cout << "enter the data for inserting in right of "<<data<<endl;
+    root->right = buildTree(root->right);
+    return root;
+    
+}
+int main(){
+    Node* root = NULL;
+    root = buildTree(root);
+    return 0;
+}
