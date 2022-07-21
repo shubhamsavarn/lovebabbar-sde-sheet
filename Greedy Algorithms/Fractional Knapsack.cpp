688fe84f0d4338e852570471800215fa88d35124
Shubham Savarn <54256934+shubhamsavarn@users.noreply.github.com>
2022-07-22 00:26:25 +0530

    Update Fractional Knapsack.cpp

--- Greedy Algorithms/Fractional Knapsack.cpp
+++ Greedy Algorithms/Fractional Knapsack.cpp
@@ -7,21 +7,22 @@ bool cmp(pair <int,int> a,pair <int,int> b){
 }
 int knapsack(pair <int,int> a[],int w,int n){
     sort(a,a+n,cmp);
-    int res = 0;
+    double res = 0.0;
     for(int i =0;i<n;i++){
-        if(a[i].second) =< w){
+        if(a[i].first <= w){
             res = res + a[i].second;
             w = w - a[i].first;
         }else{
-            res = res + (a[i].second * (double)w/a[i].first);
+            res = res + a[i].second * ((double)w/a[i].first);
+            break;
         }
     }
     return res;
 }
 int main(){
-    pair <int,int> array[] = {make_pair(50,600),{make_pair(20,500),make_pair(30,400)};
-    int w = 70;
+    pair <int, int> arr[] = {make_pair(30,120),make_pair(20,100),make_pair(10,60)};
+    int w = 50;
     int n = 3;
-    cout << knapsack(array,w,n);
+    cout << knapsack(arr,w,n);
     return 0;
 }
