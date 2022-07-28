2f6aad214f4905c8bde4e4dc958cd6f2e2af96dc
Shubham Savarn <54256934+shubhamsavarn@users.noreply.github.com>
2022-07-28 15:41:11 +0530

    Create Inorder.cpp

---
+++ BinaryTree/Inorder.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int d){
+        this->data = d;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+void inorder(Node *root){
+    if(root!=NULL){
+        inorder(root->left);
+        cout<<root->data<<" ";
+        inorder(root->right);    
+    }
+}  
+int main() {
+	
+	Node *root=new Node(10);
+	root->left=new Node(20);
+	root->right=new Node(30);
+	root->right->left=new Node(40);
+	root->right->right=new Node(50);
+	
+	inorder(root);
+}
